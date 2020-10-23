94271007fbb47c2eaf497e0179800da76b55c58d
festafolcm <carlos.fmendu@gmail.com>
2020-10-23 22:22:01 +0100

    clients connects with server using threads, but we need to keep only one list. In this code each thread/client has his own list.

--- src/TaskListClient.java
+++ src/TaskListClient.java
@@ -1,51 +1,51 @@
 import java.io.*;
+import java.net.InetAddress;
 import java.net.Socket;
 import java.util.Scanner;
 
 
 public class TaskListClient {
     public static void main(String[] args) throws IOException {
+        try{
+            Scanner scan = new Scanner(System.in);
+            InetAddress ip = InetAddress.getByName("localhost");
+            Socket skClient = new Socket(ip, 5000);
+            DataOutputStream dataOut = new DataOutputStream(skClient.getOutputStream());
+            DataInputStream dataIn = new DataInputStream(skClient.getInputStream());
 
-        Scanner scan = new Scanner(System.in);
-        DataOutputStream out;
-        DataInputStream in;
+            while(true){
+                System.out.println("You are the client, please send a message to the server");
+                String clientMessage = scan.nextLine();
 
-        Socket skClient = new Socket("127.0.0.1", 5000);
-
-        while(true){
-            out = new DataOutputStream(skClient.getOutputStream());
-            in = new DataInputStream(skClient.getInputStream());
-
-            System.out.println("You are the client, please send a message to the server");
-            String clientMessage = scan.nextLine();
-
-            switch (clientMessage){
-                case "L":
-                    out.writeUTF("L");
-                    break;
-                case "R":
-                    out.writeUTF("R");
-                    String newTask = scan.nextLine();
-                    out.writeUTF(newTask);
-                    break;
-                case "Q":
-                    out.writeUTF("Q");
-                    out.close();
-                    in.close();
-                    skClient.close();
-                    skClient = null;
-                    break;
-                default:
-                    out.writeUTF(clientMessage);
+                switch (clientMessage){
+                    case "L":
+                        dataOut.writeUTF("L");
+                        break;
+                    case "R":
+                        dataOut.writeUTF("R");
+                        String newTask = scan.nextLine();
+                        dataOut.writeUTF(newTask);
+                        break;
+                    case "Q":
+                        dataOut.writeUTF("Q");
+                        dataOut.close();
+                        dataIn.close();
+                        skClient.close();
+                        skClient = null;
+                        break;
+                    default:
+                        dataOut.writeUTF(clientMessage);
+                }
+                if (skClient != null){
+                    System.out.println("Below you can see the message from the server");
+                    String serverMessage = dataIn.readUTF();
+                    System.out.println(serverMessage);
+                } else {
+                    break; //Needed to quit the while loop and finish without exceptions erros
+                }
             }
-
-            if (skClient != null){
-                System.out.println("Below you can see the message from the server");
-                String serverMessage = in.readUTF();
-                System.out.println(serverMessage);
-            } else {
-                break; //Needed to quit the while loop and finish without exceptions erros
-            }
+        } catch (IOException e) {
+            e.printStackTrace();
         }
     }
 }
--- src/TaskListServer.java
+++ src/TaskListServer.java
@@ -7,42 +7,73 @@ import java.util.Arrays;
 
 public class TaskListServer {
     public static void main(String[] args) throws IOException {
-
-        DataOutputStream out;
-        DataInputStream in;
-
         ArrayList<String> tasks = new ArrayList();
-
         ServerSocket skServer = new ServerSocket(5000);
-        Socket client = skServer.accept(); //Server accepts the connection with the client
 
         while (true) {
-            out = new DataOutputStream(client.getOutputStream());
-            in = new DataInputStream(client.getInputStream());
+            Socket client = null;
+            try {
+                client = skServer.accept(); //Server accepts the connection with the client
+                System.out.println("A new client is connected: " + client);
+                DataOutputStream out = new DataOutputStream(client.getOutputStream());
+                DataInputStream in = new DataInputStream(client.getInputStream());
+                //Assigning new thread for this client
+                Thread newThread = new ClientHandler(client, in, out);
+                newThread.start();
 
-            String clientMessage = in.readUTF(); //Server reads the message from client
-            System.out.println(clientMessage);
-            switch (clientMessage){
-                case "L":
-                    String showList = Arrays.toString(tasks.toArray());
-                    out.writeUTF(showList);
-                    break;
-                case "R":
-                    String newTask = in.readUTF();
-                    if (newTask.length() <= 120){
-                        tasks.add(newTask);
-                        out.writeUTF("Task added succesfully");
-                    } else {
-                        out.writeUTF("Caracters cannot be over than 120");
-                    }
-                    break;
-                case "Q":
-                    out.close();
-                    in.close();
-                    client.close();
-                    break;
-                default:
-                    out.writeUTF("This option is not valid");
+            } catch (IOException e) {
+                client.close();
+                e.printStackTrace();
+            }
+        }
+    }
+}
+
+class ClientHandler extends Thread {
+    DataInputStream dataIn;
+    DataOutputStream dataOut;
+    Socket socket;
+
+    public ClientHandler(Socket socket, DataInputStream dataIn, DataOutputStream dataOut) {
+        this.socket = socket;
+        this.dataIn = dataIn;
+        this.dataOut = dataOut;
+    }
+
+    @Override
+    public void run (){
+        String clientMessage = null;
+        ArrayList<String> tasks = new ArrayList();
+
+        while(true){
+            try{
+                clientMessage = dataIn.readUTF();
+                switch (clientMessage){
+                    case "L":
+                        String showList = Arrays.toString(tasks.toArray());
+                        dataOut.writeUTF(showList);
+                        break;
+                    case "R":
+                        String newTask = dataIn.readUTF();
+                        if (newTask.length() <= 120){
+                            tasks.add(newTask);
+                            dataOut.writeUTF("Task added succesfully");
+                        } else {
+                            dataOut.writeUTF("Caracters cannot be over than 120");
+                        }
+                        break;
+                    case "Q":
+                        this.dataOut.close();
+                        this.dataIn.close();
+                        this.socket.close();
+                        break;
+                    default:
+                        dataOut.writeUTF("This option is not valid");
+                }
+
+            } catch (IOException e) {
+                e.printStackTrace();
+
             }
             if (clientMessage.equals("Q")){ //Needed to quit the while loop and finish without exceptions erros
                 //System.out.println("Server is closed");
